41c57d0ac018b9ba428f8853b405e880d863c286
keshav474 <guptakeshav0828@gmail.com>
2023-04-02 00:40:48 +0530

    authenticaion completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import Signin from "./components/Signin";
-import Signup from "./components/Signup";
-import Account from "./components/Account";
-import { AuthContextProvider } from "./context/AuthContext";
+import React from 'react';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Account from './components/Account';
+import { Route, Routes } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+
 function App() {
   return (
-    <div className="App">
+    <div>
+      <h1 className='text-center text-3xl font-bold'>
+        Security management system
+      </h1>
       <AuthContextProvider>
         <Routes>
-          <Route path="/" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/account" element={<Account />} />
+          <Route path='/' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </AuthContextProvider>
     </div>
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -17,7 +17,7 @@ const Signin = () => {
       navigate('/account')
     } catch (e) {
       setError(e.message)
-      console.log(e.message)
+      console.log(error)
     }
   };
 
